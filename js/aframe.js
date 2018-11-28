35de6a2a261760532b58767217b92fd2ec70d9af
janssenb <bram.janssen@vito.be>
2018-11-28 19:36:19 +0100

    First test

--- js/aframe.js
+++ js/aframe.js
@@ -57,12 +57,16 @@ AFRAME.registerComponent("wms", {
     this.data.height = Math.round(this.data.width * (geom.height / geom.width));
 
 
+    el.setAttribute("geometry", "buffer", false);
+   // el.setAttribute("material", "src", this.constructURL(this.data));
+    el.removeAttribute("material", "color");
+
     var texture = this.getTexture(this.data);
 
-    this.createTerrain(texture, this.getHeightData(this.data.bbox));
+    this.createTerrain(this.data, texture);
 
-    el.setAttribute("material", "src", url);
-    el.removeAttribute("material", "color");
+    /*el.setAttribute("material", "src", url);
+    el.removeAttribute("material", "color");*/
 
   },
   getCities: function(bbox){
@@ -70,56 +74,77 @@ AFRAME.registerComponent("wms", {
     console.log("EXTENT", extent);
 
   },
-  getHeightData: function(bbox) {
-    var geom = this.el.getAttribute('geometry');
-    var extent = ol.proj.transformExtent(bbox, 'EPSG:3857', 'EPSG:4326');
-    var deltaX = extent[2] - extent[0];
-    var deltaY = extent[3] - extent[1];
-    var xPxlStep = (deltaX) / (geom.width);
-    var yPxlStep = (deltaY) / (geom.height);
+  createTerrain: function(data, texture) {
 
-    var locations = [];
+    var heightData = data;
 
-    for(var x = 0; x < geom.width; x++) {
-      for(var y = 0; y < geom.height; y++) {
-        var coordX = extent[0] + (x * xPxlStep);
-        var coordY = extent[1] + (y * yPxlStep);
+    heightData.url = 'https://geoservices.informatievlaanderen.be/raadpleegdiensten/DHMV/wms';
+    heightData.layers = 'DHMVII_DSM_1m';
 
-        locations.push({"latitude": coordY, "longitude": coordX});
+    var heightUrl = this.constructURL(heightData);
+
+    var scale = 200;
+
+    var _this = this;
+
+    var canvas = document.createElement( 'canvas' );
+    canvas.width = data.width;
+    canvas.height = data.height;
+    var size = data.width * data.height;
+    var data = new Float32Array( size );
+    var context = canvas.getContext( '2d' );
+    var img = new Image;
+    img.crossOrigin = "Anonymous";
+    img.onload = function(){
+      context.drawImage(img,0,0);
+
+      for ( var i = 0; i < size; i ++ ) {
+        data[i] = 0;
       }
-    }
 
-    var data = { "locations": locations};
-    console.log(data);
+      var imgd = context.getImageData(0, 0, img.width, img.height);
+      var pix = imgd.data;
 
-    $.ajax({type: "POST",
-      url: "https://api.open-elevation.com/api/v1/lookup",
-      data: JSON.stringify(data),
-      contentType: "application/json",
-      dataType: 'json'})
-      .done(function(data){
-      console.log(data);
-    });
+      var j=0;
+      for (var i = 0; i<pix.length; i +=8) {
+        var all = pix[i]+pix[i+1]+pix[i+2];
+        data[j++] = all/(12*scale);
+      }
 
+    _this.createMesh(img.width / 2, img.height / 2, data, texture);
+
+    };
+    img.src = heightUrl;
+
+    /*return $.ajax(
+      {
+        type: "GET",
+        url: heightUrl,
+        data: JSON.stringify(data)
+      }
+    )*/
   },
   getTexture: function(data) {
     var textureLoader = new THREE.TextureLoader();
     textureLoader.crossOrigin = "Anonymous";
     return textureLoader.load(this.constructURL(data))
   },
-  createTerrain: function(texture, heightdata) {
-    var geometry = new THREE.PlaneGeometry(this.el.width, this.el.height, this.el.width / 2, this.el.height / 2);
+  createMesh: function(width, height, data, texture) {
+    var geom = this.el.getAttribute('geometry');
+    var geometry = new THREE.PlaneGeometry(geom.width, geom.height, width -1, height -1 );
     var material = new THREE.MeshLambertMaterial( { map: texture } );
-    plane = new THREE.Mesh( geometry, material );
 
     //set height of vertices
-    for ( var i = 0; i<plane.geometry.vertices.length; i++ ) {
-      plane.geometry.vertices[i].z = data[i];
+    for ( var i = 0; i< geometry.vertices.length; i++ ) {
+      geometry.vertices[i].z = data[i];
     }
+
+    this.el.getObject3D('mesh').geometry = geometry;
+    this.el.getObject3D('mesh').material = material;
+   // this.el.setAttribute("geometry", geometry);
+   // this.el.setAttribute("material", material);
   },
   constructURL:function(data){
-
-
     if(data.version.toLowerCase()=='1.3.0'){
       projParam='CRS'
     }else{
